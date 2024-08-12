18216dbe9eb57fd23c19140d2f0cfb420c9ab7ac
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-12 12:43:41 +0500

    basic

---
+++ 01_basic/src/Section.js
@@ -0,0 +1,10 @@
+function Section(){
+    return (
+        <div className="section">
+            <h2>Section</h2>
+            <p>This is a section.</p>
+        </div>
+    )
+}
+
+export default Section;
--- 01_basic/src/index.js
+++ 01_basic/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import Section from './Section';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // fragment
+  <>
     <App />
-  </React.StrictMode>
+    <Section/>
+  </>
 );
