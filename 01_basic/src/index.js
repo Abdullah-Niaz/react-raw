aec16d54833266eecbb3febc22ade1ceea50c5b0
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-14 00:12:54 +0500

    creating custom Elements

--- 01_basic/src/index.js
+++ 01_basic/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Section from './Section';
+
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Google'
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // fragment
   <>
     <App />
+    reactElement
     <Section/>
   </>
 );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank',
+  },
+  'Google'
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    reactElement
+  // <StrictMode>
+  //   {/* Uncomment the line below if you want to render the App component */}
+  //   {/* <App /> */}
+  // </StrictMode>
+);
