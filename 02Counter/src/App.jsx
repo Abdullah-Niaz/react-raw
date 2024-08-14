8ba0297d15e3ee3edb9d9a56674fe8a5fbd342bb
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-14 16:08:15 +0500

    hooks

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCount] = useState(0);
+  const addCount = () => {
+    if (counter<20){
+      counter++;
+
+    }
+    setCount(counter);
+    console.log("Increasing Value: ", Math.random());
+  };
+  const removeValue = () => {
+    if(counter != 0){
+      counter--;
+    }
+    setCount(counter);
+    console.log("Decreasing Value: ", Math.random());
+    return counter;
+  };
+  return (
+    <>
+      <h1>Counter</h1>
+      <h3>Value: {counter}</h3>
+      <button onClick={addCount}>Increase Value</button>
+
+      <button onClick={removeValue}>Decreasse Value</button>
+    </>
+  );
+}
+
+export default App;
